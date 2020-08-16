d193f19020d6f4205ab7d23ed10d2734fd3e8ec4
VicMeg <victormegir@gmail.com>
2020-08-17 02:07:14 +0300

    Upload files

---
+++ Minecraft Clone/Assets/crosshair.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crosshair : MonoBehaviour {
+
+    public Texture2D crosshairimage;
+	
+	// Update is called once per frame
+	void OnGUI ()
+    {
+        float xMin = (Screen.width / 2) - (crosshairimage.width / 2);
+        float yMin = (Screen.height / 2) - (crosshairimage.height / 2);
+        GUI.DrawTexture(new Rect(xMin, yMin, crosshairimage.width, crosshairimage.height), crosshairimage);
+    }
+}
---
+++ Minecraft Clone/Assets/get.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class get : MonoBehaviour
+{
+    public GameObject UI;
+    public GameObject CubePrefab;
+    public GameObject camera_static;
+    public GameObject camera_FPS;
+
+    public int N;
+    void Start()
+    {
+        camera_FPS.SetActive(false);
+        camera_static.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text PlaceHolder = UI.transform.GetComponent<InputField>().placeholder.GetComponent<Text>();
+
+        if (PlaceHolder == null)
+            return;
+            
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            N = int.Parse(UI.transform.GetComponent<InputField>().text.ToString());
+            //Debug.Log(N);
+            UI.SetActive(false);
+            camera_static.SetActive(false);
+            camera_FPS.SetActive(true);
+        }
+    }
+}
